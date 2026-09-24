09952504cd27818ac2f69be5fe0bb5bba29db1e5
an1-source <15633070525@wo.cn>
2026-09-24 09:39:55 +0800

    Add files via upload

---
+++ src/main/java/com/sc_24001010415/Sc24001010415Application.java
@@ -0,0 +1,13 @@
+package com.sc_24001010415;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sc24001010415Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sc24001010415Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sc_24001010415/Sc24001010415ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc_24001010415;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sc24001010415ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
